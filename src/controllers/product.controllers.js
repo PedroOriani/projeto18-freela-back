33fdc9d6ce3d7c25e022c0babce32f0325d17f41
Pedro Oriani <pedro.oriani@gmail.com>
2023-08-14 02:00:53 -0300

    Back Finalizado

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -38,6 +38,35 @@ export async function getProductById(req, res){
 
     try{
 
+        const productId = await db.query(`
+            SELECT
+                products.*,
+                users.name AS "ownerName",
+                users.phone AS phone,
+                users.city AS city,
+                users.state AS state
+            FROM products
+            JOIN users ON products."ownerId" = users.id
+            WHERE products.id = $1
+        `, [id]);
+
+        if (productId.rowCount === 0) return res.status(404).send('Produto não encontrado');
+
+        const product = productId.rows[0]
+
+        res.send({
+            id: product.id,
+            title: product.title,
+            model: product.model,
+            photo: product.photo,
+            description: product.description,
+            price: product.price,
+            quantity: product.quantity,
+            ownerName: product.ownerName,
+            phone: product.phone,
+            adress: product.city + " - " + product.state
+        })
+
     }catch (err){
         res.status(500).send(err.message)
     }
@@ -61,7 +90,15 @@ export async function getMyProducts(req, res){
 
 export async function deleteProduct(req, res){
 
+    const { id } = req.body;
+
+    const user = res.locals.user
+
     try{
+
+        await db.query(`DELETE FROM products WHERE "ownerId"=$1 AND id=$2`, [user.rows[0].id, id]);
+
+        res.sendStatus(200);
 
     }catch (err){
         res.status(500).send(err.message)
